7d71ac45dcd7901cccbd70c432c6dc71398312ac
Oscar Otero <oom@oscarotero.com>
2025-01-15 17:34:21 +0100

    debug cwd

--- src/serve.ts
+++ src/serve.ts
@@ -1,6 +1,8 @@
+console.log(Deno.cwd());
+
 const { default: site } = await import(Deno.cwd() + "/_config.ts");
 const { default: cms } = await import(Deno.cwd() + "/_cms.ts");
-console.log(Deno.cwd());
+
 import adapter from "./adapter.ts";
 
 export default await adapter({ site, cms });
